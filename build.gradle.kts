30815d30a4b82e525067a0525676baec878a3c0b
Johnny Lim <izeye@naver.com>
2020-03-19 12:41:06 +0900

    Apply org.ajoberstar.git-publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,18 @@
 plugins {
 	id("org.asciidoctor.convert") version "1.5.8"
+	id("org.ajoberstar.grgit") version "4.0.1"
+	id("org.ajoberstar.git-publish") version "2.1.3"
+}
+
+gitPublish {
+	branch.set("gh-pages")
+	contents {
+		from("$buildDir/asciidoc/html5")
+	}
+}
+
+tasks.gitPublishCommit {
+	dependsOn(tasks.asciidoctor)
 }
 
 tasks.wrapper {
